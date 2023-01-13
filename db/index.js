93179ee3e39e7abaa06f399ec410b4b46f74ebf7
Ana Tran <ana.tran323@gmail.com>
2023-01-12 19:31:20 -0600

    B4 the updateUser in part 2:Updating our users table

--- db/index.js
+++ db/index.js
@@ -1,28 +1,31 @@
-const { Client } = require('pg'); // imports the pg module
+const { Client } = require("pg"); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client("postgres://localhost:5432/juicebox-dev");
+
+async function createUser({ username, password, name, location }) {
+  const result = await client.query(
+    `
+  INSERT INTO users(username, password, name, location) 
+  VALUES($1, $2, $3, $4) 
+  ON CONFLICT (username) DO NOTHING 
+  RETURNING *;
+  `,
+    [username, password, name, location]
+  );
+
+  return result;
+}
 
 async function getAllUsers() {
   const { rows } = await client.query(
-    'SELECT id, username FROM users;'
+    "SELECT id, username, name, location FROM users;"
   );
-  return rows; 
-}
-
-async function createUser({ username, password }) {
-    const result = await client.query(`
-    INSERT INTO users(username, password) 
-    VALUES($1, $2) 
-    ON CONFLICT (username) DO NOTHING 
-    RETURNING *;
-    `, [username, password]);
-
-  return result;
+  return rows;
 }
 
 module.exports = {
   client,
   getAllUsers,
   createUser,
-}
+};
--- db/seed.js
+++ db/seed.js
@@ -1,56 +1,73 @@
-const { Console } = require('console');
-const { client, getAllUsers, createUser } = require('./index');
+const { Console } = require("console");
+const { client, getAllUsers, createUser } = require("./index");
+
+async function dropTables() {
+  try {
+    console.log("Starting to drop tables (and some beats)...");
+
+    await client.query(`
+      DROP TABLE IF EXISTS users;
+    `);
+
+    console.log("Finished droping tables");
+  } catch (error) {
+    console.error("Error dropping tables :(");
+    throw error;
+  }
+}
+
+async function createTables() {
+  try {
+    console.log("Starting to build tables...");
+
+    await client.query(`
+    CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username varchar(225) UNIQUE NOT NULL,
+      password varchar(225) NOT NULL,
+      name VARCHAR(255) NOT NULL, 
+      location VARCHAR(255) NOT NULL, 
+      active BOOLEAN DEFAULT true
+    )
+    `);
+
+    console.log("Finished building tables");
+  } catch (error) {
+    console.error("Error building tables :(");
+    throw error;
+  }
+}
 
 async function createInitialUsers() {
   try {
     console.log("Starting to create users...");
 
-    const albert = await createUser({ username: 'albert', password: 'bertie99' });
-    const sandra = await createUser({ username: 'sandra', password: '2sandy4me' });
-    const glamgal = await createUser({username: 'glamgal', password: 'soglam'})
+    const albert = await createUser({
+      username: "albert",
+      password: "bertie99",
+      name: "Al Bert",
+      location: "Sydney, Australia",
+    });
+    const sandra = await createUser({
+      username: "sandra",
+      password: "2sandy4me",
+      name: "Just Sandra",
+      location: "Ain't tellin'",
+    });
+    const glamgal = await createUser({
+      username: "glamgal",
+      password: "soglam",
+      name: "Joshua",
+      location: "Upper East Side",
+    });
 
     console.log(albert);
     console.log(sandra);
     console.log(glamgal);
 
     console.log("Finished creating users!");
-  } catch(error) {
+  } catch (error) {
     console.error("Error creating users!");
-    throw error;
-  }
-}
-
-
-async function dropTables() {
-  try {
-    console.log('Starting to drop tables (and some beats)...');
-
-    await client.query(`
-      DROP TABLE IF EXISTS users;
-    `);
-
-    console.log('Finished droping tables');
-  } catch (error) {
-    console.error('Error dropping tables :(');
-    throw error; 
-  }
-}
-
-async function createTables() {
-  try {
-    console.log('Starting to build tables...');
-
-    await client.query(`
-    CREATE TABLE users (
-      id SERIAL PRIMARY KEY,
-      username varchar(225) UNIQUE NOT NULL,
-      password varchar(225) NOT NULL
-    )
-    `);
-
-    console.log('Finished building tables')
-  } catch (error) {
-    console.error("Error building tables :(");
     throw error;
   }
 }
@@ -64,24 +81,23 @@ async function rebuildDB() {
     await createInitialUsers();
   } catch (error) {
     console.error(error);
-    throw error
-  } 
+    throw error;
+  }
 }
-
 
 async function testDB() {
   try {
-    console.log("Starting to test database...")
-    
+    console.log("Starting to test database...");
+
     // queries are promises, so we can await them
     const users = await getAllUsers();
     console.log("getAllUsers:", users);
-    
-    console.log('Finshed DB tests!');
+
+    console.log("Finshed DB tests!");
   } catch (error) {
-    console.error('Error testing database :(');
-    throw error
-  } 
+    console.error("Error testing database :(");
+    throw error;
+  }
 }
 
 rebuildDB()
